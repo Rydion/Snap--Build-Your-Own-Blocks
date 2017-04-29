442d6fb03475648d33bf3cb1e1c56686b62d34d8
Adrian Hintze <rydion00@gmail.com>
2017-04-29 21:59:10 +0200

    Now this branch only contains the changes relevant to the full version of Snap needed for Snapp.

--- gui.js
+++ gui.js
@@ -497,6 +497,29 @@ IDE_Morph.prototype.openIn = function (world) {
     } else {
         interpretUrlAnchors.call(this);
     }
+
+    // Snapp!
+    this.rawOpenProjectString(this.snapproject);
+    this.toggleAppMode(true);
+    var handle = setInterval(function () {
+        var allSpritesDone = true;
+        myself.stage.children.forEach(function (child) {
+            var allCostumesLoaded = true;
+            if (child.costumes) {
+                child.costumes.contents.forEach(function (costume) {
+                    if (typeof costume.loaded === "function") { allCostumesLoaded = false; }
+                });
+            }
+            if (!allCostumesLoaded || (child.costumes && child.costumes.length > 0 && !child.costume)) {
+                allSpritesDone = false;
+            }
+        });
+
+        if (allSpritesDone) {
+            clearInterval(handle);
+            myself.runScripts();
+        }
+    }, 100);
 };
 
 // IDE_Morph construction
